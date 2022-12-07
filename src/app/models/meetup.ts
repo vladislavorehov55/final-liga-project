25d9b68468d8b6a391665d624fedec94c853240c
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-07 16:49:29 +0300

    Added: meetup model

---
+++ src/app/models/meetup.ts
@@ -0,0 +1,13 @@
+
+export interface IMeetup {
+  id: number
+  name: string
+  description: string
+  location: string
+  target_audience: string
+  need_to_know: string
+  will_happen: string
+  reason_to_come: string
+  time: string
+  createdBy: string
+}
