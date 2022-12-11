d59e8a021ac4d41d954fbc5e63bb3c205df71d86
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-11 17:14:43 +0300

    Added: routing to auth page and meetups page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MeetupsPageComponent} from "./components/pages/meetups-page/meetups-page.component";
+import {AuthPageComponent} from "./components/pages/auth-page/auth-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MeetupsPageComponent
+  },
+  {
+    path: 'auth',
+    component: AuthPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
