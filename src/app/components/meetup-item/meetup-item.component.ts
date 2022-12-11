eede189976566614f265cffff633ceba4f6b107f
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-12 01:47:10 +0300

    Added: subscribe and unsubscribe to meetup by user

--- src/app/components/meetup-item/meetup-item.component.ts
+++ src/app/components/meetup-item/meetup-item.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {IMeetup} from "../../models/meetup";
 import {MeetupService} from "../../services/meetup/meetup.service";
 import {AuthService} from "../../services/auth/auth.service";
+import {IUser} from "../../models/user";
 
 @Component({
   selector: 'app-meetup-item',
@@ -22,8 +23,19 @@ export class MeetupItemComponent implements OnInit{
     const subscribedUsers  = this.meetup.users
     for (let user of subscribedUsers) {
       // @ts-ignore
-      if (user.id === this._authService.user.id) return true
+      if (user.id === this._authService.user.id) {
+        return false
+      }
     }
-    return false
+    return true
+  }
+  subscribe(idMeetup: number, idUser: number) {
+    this._meetupService.subscribe(idMeetup, idUser)
+  }
+  unsubscribe(idMeetup: number, idUser: number) {
+    this._meetupService.unsubscribe(idMeetup, idUser)
+  }
+  get user() {
+    return this._authService.user as IUser
   }
 }
--- src/app/services/meetup/meetup.service.ts
+++ src/app/services/meetup/meetup.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {IMeetup} from "../../models/meetup";
 import {EnvironmentService} from "../environment/environment.service";
 import {HttpClient} from "@angular/common/http";
@@ -7,6 +7,7 @@ import {map} from "rxjs";
 @Injectable()
 export class MeetupService {
   meetups: Array<IMeetup> = []
+
   constructor(private environmentService: EnvironmentService, private http: HttpClient) {
   }
 
@@ -29,5 +30,39 @@ export class MeetupService {
       }
       return meetup
     })
+  }
+
+  subscribe(idMeetup: number, idUser: number) {
+    this.http.put<IMeetup>(`${this.environmentService.environment.apiUrl}/meetup`, {idMeetup, idUser})
+      .pipe(
+        map((meetup) => {
+          for (let i = 0; i < this.meetups.length; i++) {
+            if (this.meetups[i].id === meetup.id) {
+              this.meetups[i] = meetup
+              break
+            }
+          }
+          return this.meetups
+        })
+      )
+      .subscribe((data) => this.meetups = data)
+  }
+
+  unsubscribe(idMeetup: number, idUser: number) {
+    this.http.delete<IMeetup>(`${this.environmentService.environment.apiUrl}/meetup`, {
+      body: {idMeetup, idUser}
+    })
+      .pipe(
+        map((meetup) => {
+          for (let i = 0; i < this.meetups.length; i++) {
+            if (this.meetups[i].id === meetup.id) {
+              this.meetups[i] = meetup
+              break
+            }
+          }
+          return this.meetups
+        })
+      )
+      .subscribe((data) => this.meetups = data)
   }
 }
