60d8d04dfd4e158b56730dff12be0ed3ee4c3a3e
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-15 23:41:52 +0300

    Added: logout from app

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private _authService: AuthService) {
+  }
 
+  logoutHandler() {
+    this._authService.logout()
+  }
+
+  get user() {
+    return this._authService.user
+  }
 }
