34cb4f6bade88daa603334ee5be556a2628f195c
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-17 15:21:57 +0300

    Fixed: interface that responses in getDataUsers

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {EnvironmentService} from "../environment/environment.service";
-import {IUser} from "../../models/user";
+import {IUserResponse} from "../../models/user";
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,8 @@ export class UserService {
   constructor(private _http: HttpClient, private _environmentService: EnvironmentService,) { }
 
   getDataUsers() {
-    this._http.get<IUser[]>(`${this._baseURL}`)
-      .subscribe((users: IUser[]) => {
+    this._http.get<IUserResponse[]>(`${this._baseURL}`)
+      .subscribe((users: IUserResponse[]) => {
         console.log('users', users)
       })
   }
