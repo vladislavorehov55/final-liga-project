589faffb13fa8565320b19e208dab959bad00964
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-17 19:01:30 +0300

    Added: role service and getting roles via API, chose role in form

--- src/app/components/form-user/form-user.component.ts
+++ src/app/components/form-user/form-user.component.ts
@@ -1,34 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormService} from "../../services/form/form.service";
+import {RoleService} from "../../services/role/role.service";
+import {IRole} from "../../models/role";
+import {IUser} from "../../models/user";
 
 @Component({
   selector: 'app-form-user',
   templateUrl: './form-user.component.html',
   styleUrls: ['./form-user.component.scss']
 })
-export class FormUserComponent {
-  constructor(private _formService: FormService) {
+export class FormUserComponent implements OnInit{
+  constructor(private _formService: FormService, private _roleService: RoleService) {
   }
+
+  ngOnInit() {
+    this._roleService.getDataRoles()
+  }
+
+  get roles() {
+    return this._roleService.roles
+  }
+
   get form() {
     return this._formService.form
   }
+
   get title() {
     return this._formService.title
   }
+
   get isSown() {
     return this._formService.isShow
   }
+
   get formType() {
     return this._formService.formType
   }
+
   closeFormHandler() {
     this._formService.isShow = false
   }
-  userRole = 'пользователь'
+
+  userRole: string = 'user'
+
   get chosenUserRole() {
     return this.userRole
   }
-  changeUserRoleHandler(e: any) {
-    this.userRole = e.target.textContent
+
+  changeUserRoleHandler(role: IRole) {
+    this.userRole = role.name
   }
 }
---
+++ src/app/services/role/role.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {EnvironmentService} from "../environment/environment.service";
+import {IRole} from "../../models/role";
+import {concatAll, filter, tap, toArray} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  private _baseURL: string = `${this._environmentService.environment.apiUrl}/role`
+  private _roles:IRole[] = []
+  constructor(private _http: HttpClient, private _environmentService: EnvironmentService) {
+  }
+  get roles() {
+    return this._roles
+  }
+
+  getDataRoles() {
+    this._http.get<IRole[]>(`${this._baseURL}`)
+      .pipe(
+        concatAll(),
+        filter((role: IRole) => role.name !== ''),
+        toArray()
+      )
+      .subscribe((data: IRole[]) => {
+        console.log('roles', data)
+        this._roles = data
+      })
+  }
+}
