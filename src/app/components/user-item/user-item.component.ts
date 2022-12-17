36dc943eb1a98ebaa5313bacfd7ca292172022c1
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-17 17:47:48 +0300

    Added: filling the editing form with data

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IUser, IUserResponse} from "../../models/user";
+import {UserService} from "../../services/user/user.service";
+import {FormService} from "../../services/form/form.service";
 
 @Component({
   selector: 'app-user-item',
@@ -9,6 +11,17 @@ import {IUser, IUserResponse} from "../../models/user";
 export class UserItemComponent implements OnInit{
   @Input()
   user!: IUserResponse
+  constructor(private _formService: FormService) {
+  }
   ngOnInit() {
+  }
+
+  openEditUserFormHandler() {
+    this._formService.isShow = true
+    this._formService.setUserForm('EDIT_USER', 'Редактирование пользователя', {
+      fio: this.user.fio,
+      email: this.user.email,
+      password: this.user.password
+    })
   }
 }
