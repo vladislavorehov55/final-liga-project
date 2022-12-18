5df364e4f7228ff536c6a20708faafc735c34413
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-18 22:12:59 +0300

    Added: client field validation with the help of the angular validators

--- src/app/components/pages/auth-page/auth-page.component.ts
+++ src/app/components/pages/auth-page/auth-page.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../services/auth/auth.service";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-auth-page',
@@ -22,15 +22,44 @@ export class AuthPageComponent implements OnInit {
 
   private _createForm() {
     this.form = new FormGroup({
-      login: new FormControl<string>(''),
-      password: new FormControl<string>('')
+      login: new FormControl<string>('', {
+        validators: [Validators.required, Validators.email]
+      } ),
+      password: new FormControl<string>('', {
+        validators: [Validators.required]
+      })
     })
   }
 
-
+  loginError: string = ''
+  passwordError: string = ''
+  onBlurLoginHandler() {
+    if (this.form.controls.login.errors?.['required']) {
+      this.loginError = 'Поле не заполнено'
+    }
+    else if (this.form.controls.login.errors?.['email']) {
+      this.loginError = 'Некорректная почта'
+    }
+  }
+  onFocusLoginHandler() {
+    this.loginError = ''
+    if (this.form.controls.login.invalid) {
+      this.form.controls.login.reset()
+    }
+  }
+  onBlurPasswordHandler() {
+    if (this.form.controls.password.errors) {
+      this.passwordError = 'Поле не заполнено'
+    }
+  }
+  onFocusPasswordHandler() {
+    if (this.form.controls.password.invalid) {
+      this.form.controls.password.reset()
+    }
+  }
   submitHandler(event: SubmitEvent) {
     event.preventDefault()
-    this._authService.login(this.form.get('login')?.value, this.form.get('password')?.value)
+    this._authService.login(this.form.value.login, this.form.value.password)
   }
 
 }
