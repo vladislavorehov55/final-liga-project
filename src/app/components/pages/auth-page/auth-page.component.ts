f32a46b016c818281fe9771a2a51c66f027ddc73
vladislavorehov55 <vladislavorehov55@gmail.com>
2022-12-20 01:23:58 +0300

    Fixed: auth form validation

--- src/app/components/pages/auth-page/auth-page.component.ts
+++ src/app/components/pages/auth-page/auth-page.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {AuthService} from "../../../services/auth/auth.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-auth-page',
@@ -19,7 +20,21 @@ export class AuthPageComponent implements OnInit {
   ngOnInit() {
     this._createForm()
   }
+  ngOnDestroy() {
+    this._loginSubscription.unsubscribe()
+    this._passwordSubscription.unsubscribe()
+  }
+  private _login!: string
+  get login() {
+    return this._login
+  }
+  private _password!: string
+  get password() {
+    return this._password
+  }
 
+  private _loginSubscription = new Subscription()
+  private _passwordSubscription = new Subscription()
   private _createForm() {
     this.form = new FormGroup({
       login: new FormControl<string>('', {
@@ -29,47 +44,80 @@ export class AuthPageComponent implements OnInit {
         validators: [Validators.required]
       })
     })
+    this._passwordSubscription = this.form.controls['password'].valueChanges.subscribe((value) => {
+      const {errors, untouched, invalid} = this.form.controls['password']
+      if (!invalid) {
+        this._passwordError = ''
+      }
+      if (!untouched && invalid) {
+        if (errors) {
+          if (errors['required']) {
+            this._passwordError = 'Поле не заполнено'
+          }
+        }
+      }
+    })
+    this._loginSubscription = this.form.controls['login'].valueChanges.subscribe((value) => {
+      const {errors, untouched, invalid} = this.form.controls['login']
+      if (!invalid) {
+        this._loginError = ''
+      }
+      if (!untouched && invalid) {
+        if (errors) {
+          if (errors['required']) {
+            this._loginError = 'Поле не заполнено'
+          }
+          else if (errors['email'] ) {
+            this._loginError = 'Некорректное поле'
+          }
+        }
+      }
+    })
     this.form.get('login')?.valueChanges.subscribe((value) => {
       if (value === '') {
         this.form.controls.login.reset()
       }
     })
-    this.form.get('password')?.valueChanges.subscribe((value) => {
-      if (value === '') {
-        this.form.controls.password.reset()
-      }
-    })
   }
 
-  loginError: string = ''
-  passwordError: string = ''
+  private _loginError: string = ''
+  private _passwordError: string = ''
+  get passwordError() {
+    return this._passwordError
+  }
+  get loginError() {
+    return this._loginError
+  }
   onBlurLoginHandler() {
     if (this.form.controls.login.errors?.['required']) {
-      this.loginError = 'Поле не заполнено'
+      this._loginError = 'Поле не заполнено'
     }
     else if (this.form.controls.login.errors?.['email']) {
-      this.loginError = 'Некорректная почта'
+      this._loginError = 'Некорректное поле'
     }
   }
   onFocusLoginHandler() {
-    this.loginError = ''
+    this._loginError = ''
     if (this.form.controls.login.invalid) {
       this.form.controls.login.reset()
     }
   }
   onBlurPasswordHandler() {
     if (this.form.controls.password.errors) {
-      this.passwordError = 'Поле не заполнено'
+      this._passwordError = 'Поле не заполнено'
     }
   }
   onFocusPasswordHandler() {
-    this.passwordError = ''
+    this._passwordError = ''
     if (this.form.controls.password.invalid) {
       this.form.controls.password.reset()
     }
   }
   submitHandler(event: SubmitEvent) {
     event.preventDefault()
+    if (this.form.invalid) {
+      return
+    }
     this._authService.login(this.form.value.login, this.form.value.password)
   }
 
